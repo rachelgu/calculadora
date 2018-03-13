98f37b219e377d18357f710cbda69069d1eefa70
Raquel Oliveira <aluno19994@ipt.pt>
2018-03-13 10:11:50 +0000

    Acrescento das operações e restantas funcionalidades da calculadora

--- CalculadoraComplexa/CalculadoraComplexa/Controllers/HomeController.cs
+++ CalculadoraComplexa/CalculadoraComplexa/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace CalculadoraComplexa.Controllers
         [HttpGet] // esta anotação é facultativa, pois, por defeito 
         public ActionResult Index()
         {
+            limpaCampos();
             ViewBag.Visor = 0;
             return View();
         }
@@ -20,8 +21,10 @@ namespace CalculadoraComplexa.Controllers
         public ActionResult Index(string bt, string visor){
 
             // identificar o valor da variavel 'bt'
+            string ausar = "";
             switch (bt)
             {
+                case "0":
                 case "1":
                 case "2":
                 case "3":
@@ -31,15 +34,71 @@ namespace CalculadoraComplexa.Controllers
                 case "7":
                 case "8":
                 case "9":
-                    if (visor.Equals("0")) visor = bt;
-                    else visor += bt;
-                    break;
+                    if ((bool)Session["depoisOp"]) limpaCampos();
 
+                    ausar = "num1";
+                    if (!Session["operacao"].Equals("")) { ausar = "num2"; }
+                    if (Session[ausar].Equals("0")) Session[ausar] = bt;
+                    else Session[ausar] += bt;
+                    break;
+                case "+":
+                case "-":
+                case "x":
+                case "/":
+                    Session["depoisOp"] = false;
+                    Session["operacao"] = bt;
+                    break;
+                case "=":
+                    if (Session["num2"].Equals("0") || Session["operacao"].Equals("")) break;
+                    switch(Session["operacao"])
+                    {
+                        case "+":
+                            Session["num1"] = (Double.Parse((string)Session["num1"]) + Double.Parse((string)Session["num2"])).ToString();
+                            break;
+                        case "-":
+                            Session["num1"] = (Double.Parse((string)Session["num1"]) - Double.Parse((string)Session["num2"])).ToString();
+                            break;
+                        case "*":
+                            Session["num1"] = (Double.Parse((string)Session["num1"]) * Double.Parse((string)Session["num2"])).ToString();
+                            break;
+                        case "/":
+                            Session["num1"] = (Double.Parse((string)Session["num1"]) / Double.Parse((string)Session["num2"])).ToString();
+                            break;
+                    }
+                    Session["num2"] = "0";
+                    Session["operacao"] = "";
+                    Session["depoisOp"] = true;
+                    break;
+                case ",":
+                    if ((bool)Session["depoisOp"]) limpaCampos();
+                    string a = Convert.ToString(System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+
+                    ausar = "num1";
+                    if (!Session["operacao"].Equals("")) { ausar = "num2"; }
+                    if(!((string)Session[ausar]).Contains(a))Session[ausar] += a;
+                    break;
+                case "+/-":
+                    ausar = "num1";
+                    if (!Session["operacao"].Equals("")) { ausar = "num2"; }
+                    Session[ausar] = (Double.Parse((string)Session[ausar]) * -1).ToString();
+                    break;
+                case "c":
+                    limpaCampos();
+                    break;
             }
-            
-            ViewBag.visor = visor;
-       
+
+            if (Session["operacao"].Equals("")) ViewBag.visor = Session["num1"];
+            else ViewBag.visor = Session["num2"];
             return View();
         }
+
+        private void limpaCampos()
+        {
+            Session["num1"] = "0";
+            Session["num2"] = "0";
+            Session["operacao"] = "";
+            Session["depoisOp"] = false;
+        }
     }
+
 }
